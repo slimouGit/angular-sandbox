3d21229cb5b4baf6124350c21de9e7213c9c00d9
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-07 08:12:58 +0200

    property binding

---
+++ sandbox/src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  templateUrl: './property-binding.component.html',
+  styleUrls: ['./property-binding.component.css']
+})
+export class PropertyBindingComponent {
+
+  title:string = 'Property Binding';
+  imgUrl:string = 'https://angular.io/assets/images/logos/angular/angular.svg';
+
+}
