b27c2be238661c6227cd3637b08ba10fbacd29a5
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 09:49:26 +0200

    comunicate between components via Subscription

--- sandbox/src/app/emit-parent/emit-child/emit-child.component.ts
+++ sandbox/src/app/emit-parent/emit-child/emit-child.component.ts
@@ -1,16 +1,33 @@
-import { Component,Output,EventEmitter} from '@angular/core';
+import { Component,Output,EventEmitter, OnInit, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ParentService } from '../parent-service';
 
 @Component({
   selector: 'app-emit-child',
   templateUrl: './emit-child.component.html',
   styleUrls: ['./emit-child.component.css']
 })
-export class EmitChildComponent  {
+export class EmitChildComponent implements OnInit, OnDestroy  {
   helloToParent:string = '';
   @Output() greetingEvent = new EventEmitter<string>();
 
+  haveToGoToBedNow = false;
+  private goToBedSub: Subscription;
+
+  constructor(private parentService:ParentService){}
+
   greetingParentInFunctionFromChild(hello:string){
     this.greetingEvent.emit(hello);
+  }
+
+  ngOnInit() {
+    this.goToBedSub = this.parentService.sendToBedEmitter.subscribe(didParentSentChildToBed => {
+      this.haveToGoToBedNow = didParentSentChildToBed;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.goToBedSub.unsubscribe();
   }
 
 }
--- sandbox/src/app/emit-parent/emit-parent.component.ts
+++ sandbox/src/app/emit-parent/emit-parent.component.ts
@@ -1,4 +1,5 @@
 import { Component,Input } from '@angular/core';
+import { ParentService } from './parent-service';
 
 @Component({
   selector: 'app-emit-parent',
@@ -9,8 +10,14 @@ export class EmitParentComponent  {
 
   @Input() greetigFromChild:string = '';
 
+  constructor(private parentService: ParentService){}
+
   receiveGreetingFromFunctionInParent(hello:string){
     this.greetigFromChild = hello;
+  }
+
+  sendChildToBed(){
+    this.parentService.sendToBedEmitter.next(true);
   }
 
 }
