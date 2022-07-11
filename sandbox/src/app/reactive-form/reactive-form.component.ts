4389a19cd8e81f2107ad4bc5ca020f2a84624d14
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-11 13:30:09 +0200

    clean up reactive form

--- sandbox/src/app/reactive-form/reactive-form.component.ts
+++ sandbox/src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+genders = ['male', 'female'];
+
 
 }
