989c7e7eeb3028116b672a54ec96b43e32794484
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-12 13:47:42 +0200

    put http methods in service

---
+++ sandbox/src/app/backend-communication/post.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+import { Post } from "./post.model";
+
+@Injectable({providedIn: 'root'})
+export class PostService{
+
+    dbUrl: string;
+
+    constructor(private http: HttpClient){ 
+        this.dbUrl = 'https://ng-sandbox-db-default-rtdb.firebaseio.com/';
+    }
+
+    createAndStorePost(title: string, content:string){
+        const postData: Post = { title: title, content:content };
+        this.http.post(this.dbUrl + 'posts.json', postData).subscribe(responseData => {
+            console.log(responseData);
+          });
+    }
+
+    fetchPosts(){
+        return this.http.get<{[key:string]: Post}>(this.dbUrl + 'posts.json')
+      .pipe(map(responseData => {
+        const postArray:Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key })
+          }
+        }
+        return postArray;
+      }));
+    }
+}
