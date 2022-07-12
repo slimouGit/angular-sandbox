0ac9396f3c411406e236e6dce3b8a4798638ea35
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-12 14:24:46 +0200

    adding headers

--- sandbox/src/app/backend-communication/post.service.ts
+++ sandbox/src/app/backend-communication/post.service.ts
@@ -1,44 +1,48 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, catchError } from "rxjs";
 import { Subject, throwError } from "rxjs";
 import { Post } from "./post.model";
 
-@Injectable({providedIn: 'root'})
-export class PostService{
+@Injectable({ providedIn: 'root' })
+export class PostService {
 
     dbUrl: string;
     error = new Subject();
 
-    constructor(private http: HttpClient){ 
+    constructor(private http: HttpClient) {
         this.dbUrl = 'https://ng-sandbox-db-default-rtdb.firebaseio.com/';
     }
 
-    createAndStorePost(title: string, content:string){
-        const postData: Post = { title: title, content:content };
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = { title: title, content: content };
         this.http.post(this.dbUrl + 'posts.json', postData).subscribe(responseData => {
             console.log(responseData);
-            
-          }, error => this.error.next(error.message));
+
+        }, error => this.error.next(error.message));
     }
 
-    fetchPosts(){
-        return this.http.get<{[key:string]: Post}>(this.dbUrl + 'posts.json')
-      .pipe(map(responseData => {
-        const postArray:Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({ ...responseData[key], id: key })
-          }
-        }
-        return postArray;
-      }), catchError(errorRes => {
-            return throwError(errorRes);
-      })
-      );
+    fetchPosts() {
+        return this.http.get<{ [key: string]: Post }>(this.dbUrl + 'posts.json',
+                {
+                    headers: new HttpHeaders({'Custom-Header': 'Hello'})
+                }
+            )
+            .pipe(map(responseData => {
+                const postArray: Post[] = [];
+                for (const key in responseData) {
+                    if (responseData.hasOwnProperty(key)) {
+                        postArray.push({ ...responseData[key], id: key })
+                    }
+                }
+                return postArray;
+            }), catchError(errorRes => {
+                return throwError(errorRes);
+            })
+            );
     }
-    
-    onDeletePosts(){
+
+    onDeletePosts() {
         return this.http.delete(this.dbUrl + 'posts.json');
     }
 }
