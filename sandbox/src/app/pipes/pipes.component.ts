719e3074fff3094d654ec215c6f50a80afa52785
Salim Oussayfi <salim.oussayfi-cic@ibm.com>
2022-07-12 08:22:03 +0200

    some example values

--- sandbox/src/app/pipes/pipes.component.ts
+++ sandbox/src/app/pipes/pipes.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipesComponent implements OnInit {
 
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
