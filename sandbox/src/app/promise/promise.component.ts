cb626fd8d91e49b1535e670e6f7bc1ac376ff84c
oussa <box@slimou.net>
2022-08-13 17:29:04 +0200

    get value from ui

--- sandbox/src/app/promise/promise.component.ts
+++ sandbox/src/app/promise/promise.component.ts
@@ -1,39 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-promise',
   templateUrl: './promise.component.html',
   styleUrls: ['./promise.component.css']
 })
-export class PromiseComponent implements OnInit {
+export class PromiseComponent {
 
-  constructor() { }
-  
-  ngOnInit() {
-    this.callFunction().then(
+  amount: number;
+  result: string;
+
+  constructor() {
+  }
+
+  checkNumber() {
+    this.callFunction(this.amount).then(
       (val) => console.log(val),
       (err) => console.error(err)
-    );    
-
+    );
   }
-  callFunction() {
-    let error = this.doSomething(40);
-    return new Promise((resolve, reject) => {      
+
+  callFunction(amount: number) {
+    let error = this.doSomething(amount);
+    console.log(this.amount);
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (error) {
-          reject('error'); 
+          this.result = "Not so good";
+          reject('error');
         } else {
-          resolve('done'); 
+          this.result = "Very good good";
+          resolve('done');
         }
       }, 1000);
     });
-}
+  }
 
-doSomething(x:number) {
-  if(x<42){
-    return new Error('Oh no!');
-  }  
-}
+  doSomething(x: number) {
+    if (x < 42) {
+      return new Error('Oh no!');
+    }
+  }
 }
 
 
